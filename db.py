74bd22b85a299f2ad0b77df07883cb63e55f5775
darylkangjh <daryl.kangjh@gmail.com>
2020-08-19 05:26:22 +0000

    create all review

--- db.py
+++ db.py
@@ -14,12 +14,26 @@ client = pymongo.MongoClient(MONGO_URI)
 db = client['EatRank']
 
 # Routes Below 
-@app.route('/all')
+@app.route('/')
 def show_reviews():
     all_reviews = db.review.find()
 
     return render_template('show/all_review.template.html', 
                     review = all_reviews)
+
+@app.route('/show-restaurants')
+def show_restaurants():
+    all_restaurants = db.restaurant.find()
+
+    return render_template('show/all_restaurant.template.html', 
+                    restaurant = all_restaurants)
+
+@app.route('/create-review')
+def create_reviews():
+    all_reviews = db.review.find()
+
+    return render_template('show/all_review.template.html', 
+                    review = all_reviews)  
 
 # "magic code" -- boilerplate
 if __name__ == '__main__':
