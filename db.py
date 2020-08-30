a6cc21ea35675d8a8885320a063cac7ed7966b0f
darylkangjh <daryl.kangjh@gmail.com>
2020-08-30 15:30:42 +0000

    restaurant menuitem addedneed to iterate through array to show all items

--- db.py
+++ db.py
@@ -76,7 +76,20 @@ def show_restaurants():
     all_restaurants = db.restaurant.find()
 
     return render_template('show/all_restaurant.template.html', 
-                    restaurant =all_restaurants)
+                           restaurant =all_restaurants)
+
+@app.route('/show-restaurants/<restaurant_id>')
+def show_one_restaurant(restaurant_id):
+    one_restaurant=db.restaurant.find_one({
+        '_id':ObjectId(restaurant_id)
+    })
+
+    menu_item = db.menu_item.find()
+
+    return render_template('show/one_restaurant.template.html', restaurant=one_restaurant, menu=menu_item )
+
+
+
 
 # CREATE Routes Below 
 @app.route('/create-restaurant')
@@ -110,57 +123,7 @@ def process_create_reviews():
     db.restaurant.insert_one(new_record)
     return redirect(url_for('show_restaurants'))  
 
-# Show for all customer (admin)
-@app.route('/show-customers')
-def show_customer():
-    all_customer = db.customer.find()
 
-    return render_template('show/all_customer.template.html', 
-                    customer = all_customer)
-
-# Customer account and all that he/she commented on
-@app.route('/show-customer-account/<customer_id>')
-def show_customer_account(customer_id):
-
-    customer = db.customer.find_one({
-        '_id': ObjectId(customer_id),
-    })
-   
-    all_reviews = db.review.find({
-        'customer': ObjectId(customer_id),
-    })
-    return render_template('show/one_customer.template.html', customer=customer, 
-                           review=all_reviews)
-
-# Create a customer account
-@app.route('/create-customer')
-def create_customers():
-    all_customer = db.customer.find()
-
-    return render_template('create/create_customer.template.html', 
-                    customer = all_customer)  
-
-@app.route('/create-customer', methods=['POST'])
-def process_create_customers():
-    
-    # Get Information from form 
-    name = request.form.get('name')
-    contact = request.form.get('contact')
-    email = request.form.get('email')
-    password = request.form.get('password')
-
-    # Do validation later (focus on functionality first)
-
-    # Insert new customer 
-    new_record = {
-    'name': name,
-    'email': email,
-    'contact': contact,
-    'password': password,
-    }
-
-    db.customer.insert_one(new_record)
-    return redirect(url_for())
 
 # Customer Login 
 @app.route('/login')
@@ -196,6 +159,58 @@ def logout():
 def secret():
     return "Secret Area"
 
+# Show for all customer (admin)
+@app.route('/show-customers')
+def show_customer():
+    all_customer = db.customer.find()
+
+    return render_template('show/all_customer.template.html', 
+                    customer = all_customer)
+
+# Customer account and all that he/she commented on
+@app.route('/show-customer-account/<customer_id>')
+def show_customer_account(customer_id):
+
+    customer = db.customer.find_one({
+        '_id': ObjectId(customer_id),
+    })
+   
+    all_reviews = db.review.find({
+        'customer': ObjectId(customer_id),
+    })
+    return render_template('show/one_customer.template.html', customer_id=customer_id, 
+                           review=all_reviews)
+
+# Create a customer account
+@app.route('/create-customer')
+def create_customers():
+    all_customer = db.customer.find()
+
+    return render_template('create/create_customer.template.html', 
+                    customer = all_customer)  
+
+@app.route('/create-customer', methods=['POST'])
+def process_create_customers():
+
+    # Get Information from form 
+    name = request.form.get('name')
+    contact = request.form.get('contact')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # Do validation later (focus on functionality first)
+
+    # Insert new customer 
+    new_record = {
+    'name': name,
+    'email': email,
+    'contact': contact,
+    'password': password
+    }
+
+    db.customer.insert_one(new_record)
+
+    return redirect(url_for("/login"))
 
 # Restaurants below
 @app.route('/restuarants/menu/<restaurant_id>')
@@ -204,10 +219,12 @@ def show_add_menu_items(restaurant_id):
         '_id':ObjectId(restaurant_id)
     })
 
-    return render_template('create/create_menuItems.template.html', restaurant = restaurant)
+    return render_template('create/create_menuItems.template.html', 
+                           restaurant = restaurant)
 
 @app.route('/restuarants/menu/<restaurant_id>', methods =["POST"])
 def add_menu_items(restaurant_id):
+
     name = request.form.get('item-name')
     shortDes = request.form.get('short-des')
 
@@ -241,12 +258,12 @@ def create_review():
     all_reviews = db.review.find()
 
     return render_template('create/create_.template.html', 
-                    review = all_reviews)  
+                            review = all_reviews)  
 
 
 @app.route('/create-review', methods=['POST'])
 def process_create_review():
-    
+
     # Get Information from form 
     title = request.form.get('title')
     review = request.form.get('review')
@@ -270,8 +287,7 @@ def process_create_review():
     }
 
     db.restaurant.insert_one(new_review)
-    return redirect(url_for('show_restaurants'))  
-
+    return redirect(url_for('show_restaurants'))
 
 
 
